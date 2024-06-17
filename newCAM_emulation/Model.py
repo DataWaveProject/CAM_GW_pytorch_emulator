28dd18fca383274ea632750d1ee873f80e5ac023
Surbhi Goel <surbhigoel77@github.com>
2024-06-17 13:01:07 +0100

    Updated loaddata, Model, train files and removed NN_pred, and added a main file

--- newCAM_emulation/Model.py
+++ newCAM_emulation/Model.py
@@ -1,37 +1,29 @@
+import torch
 """Neural Network model for the CAM-EM."""
 
 import netCDF4 as nc
 import numpy as np
-import scipy.stats as st
-import torch
-import xarray as xr
 from torch import nn
-from torch.nn.utils import prune
-from torch.utils.data import DataLoader, Dataset
-
+from torch.utils.data import Dataset
 
 
 # Required for feeding the data iinto NN.
 class myDataset(Dataset):
-    """
-    Dataset class for loading features and labels.
-
-    Args:
-        X (numpy.ndarray): Input features.
-        Y (numpy.ndarray): Corresponding labels.
-    """
-
     def __init__(self, X, Y):
-        """Create an instance of myDataset class."""
+        """
+        Parameters:
+            X (tensor): Input data.
+            Y (tensor): Output data.
+        """
         self.features = torch.tensor(X, dtype=torch.float64)
         self.labels = torch.tensor(Y, dtype=torch.float64)
 
     def __len__(self):
-        """Return the number of samples in the dataset."""
+        """Function that is called when you call len(dataloader)"""
         return len(self.features.T)
 
     def __getitem__(self, idx):
-        """Return a sample from the dataset."""
+        """Function that is called when you call dataloader"""
         feature = self.features[:, idx]
         label = self.labels[:, idx]
 
@@ -52,31 +44,93 @@ class FullyConnected(nn.Module):
     """
     Fully connected neural network model.
 
-    The model consists of multiple fully connected layers with SiLU activation function.
+    Attributes
+    ----------
+    linear_stack : nn.Sequential
+        Sequential container of linear layers and activation functions.
+    """
+    def __init__(self):
+        super(FullyConnected, self).__init__()
+        ilev = 93
+        hidden_layers = 8
+        hidden_size = 500
+        layers = []
+        input_size = 8 * ilev + 4
+        for _ in range(hidden_layers):
+            layers.append(nn.Linear(input_size, hidden_size, dtype=torch.float64))
+            layers.append(nn.SiLU())
+            input_size = hidden_size
+        layers.append(nn.Linear(hidden_size, 2 * ilev, dtype=torch.float64))
+        self.linear_stack = nn.Sequential(*layers)
+
+    def forward(self, X):
+        """
+        Forward pass through the network.
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor.
+        """
+        return self.linear_stack(X)
+
+class EarlyStopper:
+    """
+    Early stopping utility to stop training when validation loss doesn't improve.
+
+    Parameters
+    ----------
+    patience : int, optional
+        Number of epochs to wait before stopping (default is 1).
+    min_delta : float, optional
+        Minimum change in the monitored quantity to qualify as an improvement (default is 0).
 
     Attributes
     ----------
-        linear_stack (torch.nn.Sequential): Sequential container for layers.
+    patience : int
+        Number of epochs to wait before stopping.
+    min_delta : float
+        Minimum change in the monitored quantity to qualify as an improvement.
+    counter : int
+        Counter for the number of epochs without improvement.
+    min_validation_loss : float
+        Minimum validation loss recorded.
     """
 
-    def __init__(self):
-        """Create an instance of FullyConnected NN model."""
-        super(FullyConnected, self).__init__()
-        self.normalization = NormalizationLayer(mean, std)
-        self.ilev = ilev
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
 
-        layers = []
-        layers.append(nn.Linear(8 * ilev + 4, 500))
-        layers.append(nn.SiLU())
-        
-        num_layers = 10  # Example: Change this to the desired number of hidden layers
-        for _ in range(num_layers):
-            layers.append(nn.Linear(500, 500))
-            layers.append(nn.SiLU())
+    def early_stop(self, validation_loss, model=None):
+        """
+        Check if training should be stopped early.
 
-        layers.append(nn.Linear(500, 2 * ilev))
-        self.linear_stack = nn.Sequential(*layers)
+        Parameters
+        ----------
+        validation_loss : float
+            Current validation loss.
+        model : nn.Module, optional
+            Model to save if validation loss improves (default is None).
 
-    def forward(self, x):
-        x = self.normalization(x)
-        return self.linear_stack(x)
+        Returns
+        -------
+        bool
+            True if training should be stopped, False otherwise.
+        """
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+            if model is not None:
+                torch.save(model.state_dict(), 'conv_torch.pth')
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
--- newCAM_emulation/loaddata.py
+++ newCAM_emulation/loaddata.py
@@ -1,52 +1,185 @@
-#### These data, mean, std values are to be passed to Model.py, where the data is first normalised and 
-#### then passed to data_loader in loaddata.py and then it is passed to the model.
-
+import os
+import re
+import torch
 import numpy as np
+import netCDF4 as nc
 
-ilev = 93
-dim_NN =int(8*ilev+4)
-dim_NNout =int(2*ilev)
+features = ['PS', 'Z3', 'U', 'V', 'T', 'lat', 'lon', 'DSE', 'RHOI', 'NETDT', 'NM', 'UTGWSPEC', 'VTGWSPEC']
+
+def load_variables(directory_path, variable_names, startfile, endfile):
+    """
+    Loads specified variables from NetCDF files in the given directory.
+
+    Parameters
+    ----------
+    directory_path : str
+        Path to the directory containing NetCDF files.
+    variable_names : list of str
+        List of variable names to load.
+    startfile : int
+        Starting file number.
+    endfile : int
+        Ending file number.
+
+    Returns
+    -------
+    dict
+        Dictionary containing loaded variables data.
+    """
+    variable_mapping = {'NM': 'NMBV'}
+    variable_data = {}
+    pattern = re.compile(r'^newCAM_demo_sub_\d{startfile,endfile}$')
+
+    for file_name in os.listdir(directory_path):
+        if file_name.startswith('newCAM_demo_sub_'):
+            file_path = os.path.join(directory_path, file_name)
+            with nc.Dataset(file_path) as dataset:
+                for var_name in variable_names:
+                    mapped_name = variable_mapping.get(var_name, var_name)
+                    if mapped_name in dataset.variables:
+                        var_data = dataset[mapped_name][:]
+                        variable_data[var_name] = var_data
+
+    return variable_data
+
+def load_mean_std(file_path_mean, file_path_std, variable_names):
+    """
+    Loads mean and standard deviation values for specified variables from files.
+
+    Parameters
+    ----------
+    file_path_mean : str
+        Path to the file containing mean values.
+    file_path_std : str
+        Path to the file containing standard deviation values.
+    variable_names : list of str
+        List of variable names.
+
+    Returns
+    -------
+    tuple of dict
+        Dictionaries containing mean and standard deviation values.
+    """
+    mean_data = np.load(file_path_mean)
+    std_data = np.load(file_path_std)
+    mean_dict = {var_name: mean_data[var_name] for var_name in variable_names}
+    std_dict = {var_name: std_data[var_name] for var_name in variable_names}
+    return mean_dict, std_dict
+
+def normalize_data(variable_data, mean_values, std_values):
+    """
+    Normalizes the data using mean and standard deviation values.
+
+    Parameters
+    ----------
+    variable_data : dict
+        Dictionary containing the variable data.
+    mean_values : dict
+        Dictionary containing mean values.
+    std_values : dict
+        Dictionary containing standard deviation values.
+
+    Returns
+    -------
+    dict
+        Dictionary containing normalized data.
+    """
+    normalized_data = {}
+    for var_name, var_data in variable_data.items():
+        if var_name in mean_values and var_name in std_values:
+            mean = mean_values[var_name]
+            std = std_values[var_name]
+            normalized_var_data = (var_data - mean) / std
+            normalized_data[var_name] = normalized_var_data
+    return normalized_data
 
 
-"""Read the data and the corresponding mean and std deviation"""
+def data_loader(variable_names, normalized_data, ilev):
+    """
+    Prepares the data for training by organizing it into input and output arrays.
 
-"""Iterating through the data files"""
-s_list = list(range(1, 6))
-for iter in s_list:
-    filename = "Demodata/Convection/newCAM_demo_sub_" + str(iter).zfill(1) + ".nc"  # data file
-    print('working on: ', filename)
-    fm = np.load('Demodata/mean_demo_sub.npz')  # mean file
-    fs = np.load('Demodata/std_demo_sub.npz')   # std deviation file
+    Parameters
+    ----------
+    variable_names : list of str
+        List of variable names.
+    normalized_data : dict
+        Dictionary containing normalized data.
+    ilev : int
+        Number of vertical levels.
 
+    Returns
+    -------
+    tuple of np.ndarray
+        Input and output arrays for training.
+    """
+    Ncol = normalized_data[variable_names[1]].shape[2]
+    dim_NN = int(8 * ilev + 4)  
+    dim_NNout = int(2 * ilev)
+    x_train = np.zeros([dim_NN, Ncol])
+    y_train = np.zeros([dim_NNout, Ncol])
+    target_var = ['UTGWSPEC', 'VTGWSPEC']
+    y_index = 0
+    x_index = 0
+    for var_name, var_data in normalized_data.items():
+        var_shape = var_data.shape
+        if var_name in target_var:
+            y_train[y_index * ilev:(y_index + 1) * ilev, :] = var_data.reshape(ilev, Ncol)
+            y_index += 1
+        elif len(var_shape) == 2:
+            x_train[x_index, :] = var_data
+        elif len(var_shape) == 3:
+            new_ilev = var_shape[1]
+            x_train[x_index:x_index + new_ilev, :] = var_data
+        x_index += 1
+    return x_train, y_train
 
+class MyDataset(torch.utils.data.Dataset):
+    """
+    Custom Dataset for loading features and labels.
 
+    Parameters
+    ----------
+    X : np.ndarray
+        Feature data.
+    Y : np.ndarray
+        Label data.
 
+    Attributes
+    ----------
+    features : torch.Tensor
+        Tensor containing the feature data.
+    labels : torch.Tensor
+        Tensor containing the label data.
+    """
+    def __init__(self, X, Y):
+        self.features = torch.tensor(X, dtype=torch.float64)
+        self.labels = torch.tensor(Y, dtype=torch.float64)
 
-def data_loader (U,V,T, DSE, NM, NETDT, Z3, RHOI, PS, lat, lon, UTGWSPEC, VTGWSPEC):
-  """ Function to iterate over the data read by the above part of code"""
-  Ncol = U.shape[1]
-  x_train = np.vstack([
-        U.reshape(-1, Ncol),
-        V.reshape(-1, Ncol),
-        T.reshape(-1, Ncol),
-        DSE.reshape(-1, Ncol),
-        NM.reshape(-1, Ncol),
-        NETDT.reshape(-1, Ncol),
-        Z3.reshape(-1, Ncol),
-        RHOI.reshape(-1, Ncol),
-        PS.reshape(1, Ncol),
-        lat.reshape(1, Ncol),
-        lon.reshape(1, Ncol)
-    ])
+    def __len__(self):
+        """
+        Returns the length of the dataset.
 
-  y_train = np.vstack([
-        UTGWSPEC.reshape(-1, Ncol),
-        VTGWSPEC.reshape(-1, Ncol)
-    ])
+        Returns
+        -------
+        int
+            Length of the dataset.
+        """
+        return len(self.features.T)
 
-  return x_train,y_train
+    def __getitem__(self, idx):
+        """
+        Returns a single sample from the dataset.
 
+        Parameters
+        ----------
+        idx : int
+            Index of the sample.
 
-
-
-
+        Returns
+        -------
+        tuple of torch.Tensor
+            Feature and label tensors for the sample.
+        """
+        feature = self.features[:, idx]
+        label = self.labels[:, idx]
+        return feature, label
