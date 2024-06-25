8b2a8a2659327d43a52fddcd01e46024a9401888
Surbhi Goel <surbhigoel77@github.com>
2024-06-25 21:35:31 +0100

    Changed hard coded input output values to variables

--- newCAM_emulation/Model.py
+++ newCAM_emulation/Model.py
@@ -15,15 +15,24 @@ class FullyConnected(nn.Module):
         Sequential container of linear layers and activation functions.
     """
 
-    def __init__(self, ilev=93, hidden_layers=8, hidden_size=500):
+    def __init__(
+        self, ilev=93, in_ver=8, in_nover=4, out_ver=2, hidden_layers=8, hidden_size=500
+    ):
         super(FullyConnected, self).__init__()
+        self.ilev = ilev
+        self.in_ver = in_ver
+        self.in_nover = in_nover
+        self.out_ver = out_ver
+        self.hidden_layers = hidden_layers
+        self.hidden_size = hidden_size
+
         layers = []
-        input_size = 8 * ilev + 4  
+        input_size = in_ver * ilev + in_nover
         for _ in range(hidden_layers):
             layers.append(nn.Linear(input_size, hidden_size, dtype=torch.float64))
             layers.append(nn.SiLU())
             input_size = hidden_size
-        layers.append(nn.Linear(hidden_size, 2 * ilev, dtype=torch.float64))
+        layers.append(nn.Linear(hidden_size, out_ver * ilev, dtype=torch.float64))
         self.linear_stack = nn.Sequential(*layers)
 
     def forward(self, X):
